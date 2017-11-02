118b536b94ea01af687fc0d249341a32a9b57ab4
Andrew Guinness <aaguinness@gmail.com>
2017-11-02 18:11:43 -0400

    final exercise 9

--- exercise 9.py
+++ exercise 9.py
@@ -83,22 +83,26 @@ print 1 - scipy.stats.chi2.cdf(x=D, df=1) # insig
 
 
 #2
+#import mu and S estimates of M. marinum
 bakturriuh = pandas.read_csv("MmarinumGrowth.csv")
 
+#Maximum Likelihood model to estimate max growth rate and half-sat constant
 def nullbac(p, obs):
     mu_max = p[0]
     K = p[1]
     sigma = p[2]
     
-    mu = mu_max * (bakturriuh.S / bakturriuh.S + K)
+    mu = mu_max * (bakturriuh.S / (bakturriuh.S + K))
     nll = -1*norm(mu, sigma).logpdf(bakturriuh.u).sum()
     return nll
 
 guesslist = numpy.array([1,1,1]) #not a guestlist
 
+#optimize MLL
 BacMLL = minimize(nullbac, guesslist, method="Nelder-Mead", options={'disp':True}, args=bakturriuh)
 print "Bacterial growth estimates"
-print BacMLL
+print BacMLL.x
+
 
 #3
 
